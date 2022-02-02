61b1f1201577f5d98f1aaf466b07819359f36f06
zuleidy <zuleidyrg@gmail.com>
2022-02-02 20:35:20 +0100

    test2

--- MoogleEngine/Moogle.cs
+++ MoogleEngine/Moogle.cs
@@ -5,6 +5,8 @@ namespace MoogleEngine;
 
 public static class Moogle
 {
+
+    //testt
     public static SearchResult Query(string query) {
         // Modifique este método para responder a la búsqueda
 
